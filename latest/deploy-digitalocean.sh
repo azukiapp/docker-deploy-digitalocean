e03811d1b785edfb30287be61a68069b01c246ad
Felipe Arenales <fearenales@gmail.com>
2015-09-30 18:20:37 -0300

    Changing how this image is built over azukiapp/deploy. Now the entrypoint is the same, but this image calls it with an extra argument (--provider)

--- latest/deploy-digitalocean.sh
+++ latest/deploy-digitalocean.sh
@@ -1,17 +1,5 @@
-#! /bin/sh
-
-abs_dir() {
-  cd "${1%/*}"; link=`readlink ${1##*/}`;
-  if [ -z "$link" ]; then pwd; else abs_dir $link; fi
-}
-ROOT_PATH=`abs_dir ${BASH_SOURCE:-$0}`
-cd ${ROOT_PATH}
-
-. ${ROOT_PATH}/deploy-setup.sh
-
-set -e
-
 export LOCAL_DOT_SSH_PATH=${LOCAL_ROOT_DOT_SSH_PATH}
+# TO BE FIXED!
 export PUBLIC_KEY="$( cat ${LOCAL_ROOT_DOT_SSH_PATH}/*.pub )"
 export REMOTE_HOST_ADDR_FILE="ip_addr"
 
@@ -19,15 +7,3 @@ python -u setup.py
 
 export REMOTE_HOST=`cat ${REMOTE_HOST_ADDR_FILE}`
 rm -f ${REMOTE_HOST_ADDR_FILE}
-
-RETRY=0; MAX_RETRY=10
-until [ ${RETRY} -ge ${MAX_RETRY} ]; do
-  quiet nc -w 10 ${REMOTE_HOST} 22 && break
-  RETRY=`expr ${RETRY} + 1`
-  echo "Server not accepting SSH connections yet. Retrying... (${RETRY}/${MAX_RETRY})"
-  sleep 2
-done
-
-[ ${RETRY} -ge ${MAX_RETRY} ] && echo "Failed to connect to server. Try again later." && exit 1
-
-. ${ROOT_PATH}/deploy-run.sh
